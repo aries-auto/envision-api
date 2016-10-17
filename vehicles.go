fcdae7321bb89446b503426ae74a8f04e77fc585
Alex Ninneman alex@ninneman.org <alex@ninneman.org>
2016-10-17 16:38:14 -0500

    started on simplifying getting an image to one call

--- vehicles.go
+++ vehicles.go
@@ -5,18 +5,26 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-// Vehicle Stores the iConMedia Vehicle
-type Vehicle struct {
+// IconVehicle Stores the iConMedia Vehicle
+type IconVehicle struct {
 	ID       string `json:"intVehicleID"`
 	Year     string `json:"intYear"`
 	Make     string `json:"strMake"`
 	Model    string `json:"strModel"`
 	BodyType string `json:"strBodyType"`
 	ImageURL string `json:"strImageURL"`
+}
+
+type Vehicle struct {
+	Identifier int    `json:"id"`
+	Year       string `json:"year"`
+	Make       string `json:"make"`
+	Model      string `json:"model"`
 }
 
 // IconImage represents the iConMedia image object definition.
@@ -41,10 +49,17 @@ type Image struct {
 	ColorOptions []ColorOption `json:"colors"`
 }
 
+type ImageResponse struct {
+	RequestResult
+	Vehicle      Vehicle  `json:"vehicle"`
+	Image        Image    `json:"image"`
+	MappableSKUS []string `json:"mappable"`
+}
+
 // ProductVehicleResponse Contains the response data from a Product Vehicle match
 type ProductVehicleResponse struct {
 	RequestResult
-	Vehicles []Vehicle `json:"Vehicles"`
+	Vehicles []IconVehicle `json:"Vehicles"`
 }
 
 // VehicleImageResponse represents the visual representation of a provided
@@ -132,6 +147,98 @@ func MatchFitment(c Config, vehicleID int, productIDs ...string) (*FitmentRespon
 	return &f, f.Verify()
 }
 
+func GetImage(c Config, yearStr string, makeStr string, modelStr string, colorID int, skus []string) (*ImageResponse, error) {
+	var vehicle *IconVehicle
+	var imgResponse *VehicleImageResponse
+	var allSKUS []string
+	var err error
+
+	vehicle, allSKUS, err = getFullestVehicle(c, yearStr, makeStr, modelStr, colorID, skus)
+	if err != nil {
+		return nil, err
+	}
+
+	imgResponse, err = GetVehicleImage(c, vehicle.ID, colorID, skus)
+	if err != nil {
+		// try again with no color
+		imgResponse, err = GetVehicleImage(c, vehicle.ID, 0, skus)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if len(imgResponse.Images) == 0 {
+		return nil, fmt.Errorf("no image available for %s %s %s in color %d with parts %+v", yearStr, makeStr, modelStr, colorID, skus)
+	}
+
+	var resp ImageResponse
+	resp.MappableSKUS = allSKUS
+	sort.Strings(resp.MappableSKUS)
+	resp.Image = imgResponse.Images[0]
+	resp.Vehicle = Vehicle{
+		Year:  yearStr,
+		Make:  makeStr,
+		Model: modelStr,
+	}
+
+	resp.Vehicle.Identifier, _ = strconv.Atoi(vehicle.ID)
+
+	return &resp, nil
+}
+
+func getFullestVehicle(c Config, yearStr string, makeStr, modelStr string, colorID int, skus []string) (*IconVehicle, []string, error) {
+	var err error
+	var vResponse *ProductVehicleResponse
+
+	// get the trim options for a year/make/model
+	vResponse, err = GetVehicleByYearMakeModel(c, yearStr, makeStr, modelStr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var max = 0
+	var maxProductVehicle *IconVehicle
+	var allSKUS []string
+	for _, v := range vResponse.Vehicles {
+		var prodResponse *VehicleProductResponse
+		var intVehicleID int
+		intVehicleID, err = strconv.Atoi(v.ID)
+		if err != nil {
+			continue
+		}
+		prodResponse, err = GetVehicleProducts(c, intVehicleID)
+		if err != nil {
+			continue
+		}
+
+		for _, pn := range prodResponse.Numbers {
+			if !contains(allSKUS, pn.Number) {
+				allSKUS = append(allSKUS, pn.Number)
+			}
+		}
+
+		if len(skus) > 0 {
+			var matched = 0
+			for _, pn := range prodResponse.Numbers {
+				if contains(skus, pn.Number) {
+					matched++
+				}
+			}
+
+			if matched != len(skus) {
+				continue
+			}
+		}
+
+		if len(prodResponse.Numbers) > max {
+			max = len(prodResponse.Numbers)
+			maxProductVehicle = &v
+		}
+	}
+
+	return maxProductVehicle, allSKUS, nil
+}
+
 // GetVehicleByYearMakeModel Returns the Vehicle(s) that match a given year, make, and model
 func GetVehicleByYearMakeModel(c Config, yearStr, makeStr, modelStr string) (*ProductVehicleResponse, error) {
 	vals := url.Values{}
@@ -166,13 +273,13 @@ func GetVehicleByYearMakeModel(c Config, yearStr, makeStr, modelStr string) (*Pr
 
 // GetVehicleImage returns the an image that works with the provided
 // vehicle, color, and SKU mapping.
-func GetVehicleImage(c Config, vehicleID string, colorID string, skus []string) (*VehicleImageResponse, error) {
+func GetVehicleImage(c Config, vehicleID string, colorID int, skus []string) (*VehicleImageResponse, error) {
 	vals := url.Values{}
 	vals.Add("usejson", "1")
 	vals.Add("uid", c.UserID)
 	vals.Add("vehicle", vehicleID)
 	vals.Add("part", strings.Join(skus, ","))
-	vals.Add("colorID", colorID)
+	vals.Add("color", strconv.Itoa(colorID))
 
 	resp, err := http.Get(
 		fmt.Sprintf(
@@ -237,4 +344,14 @@ func (i IconImage) transform(c Config) (*Image, error) {
 	}
 
 	return &img, nil
+}
+
+func contains(skus []string, sku string) bool {
+	for _, s := range skus {
+		if strings.Compare(s, sku) == 0 {
+			return true
+		}
+	}
+
+	return false
 }
